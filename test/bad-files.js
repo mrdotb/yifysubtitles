5e375f758148b72bc0bf60a450d5fb83ad4bf4be
mrdotb <baptiste.chaleil@gmail.com>
2016-12-21 00:39:33 +0100

    Add test for bad files.

---
+++ test/bad-files.js
@@ -0,0 +1,10 @@
+import fs from 'fs';
+import test from 'ava';
+
+test('Test with subtitle containing MACOSX dir', async t => {
+
+});
+
+test('Test with subtitle containing non srt files', async t => {
+
+});
