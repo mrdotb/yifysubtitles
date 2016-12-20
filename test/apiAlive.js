8b0c46effd3cfc5712ae58976b84e6e718782954
mrdotb <baptiste.chaleil@gmail.com>
2016-12-20 01:10:20 +0100

    use got instead of rp

--- test/apiAlive.js
+++ test/apiAlive.js
@@ -1,13 +1,8 @@
 import test from 'ava';
-import rp from 'request-promise';
+import got from 'got';
 
-test('api-yifysubtitles is Alive', async t => {
-	const options = {
-		method: 'GET',
-		uri: 'http://api.yifysubtitles.com',
-		resolveWithFullResponse: true
-	};
+test('http://api.yifysubtitles.com should be alive', async t => {
+	const res = await got(`http://api.yifysubtitles.com`);
 
-	const res = await rp(options);
 	t.is(res.statusCode, 200);
 });
