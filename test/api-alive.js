5a93867185e9138142ceceba5242eca06a470ebd
mrdotb <baptiste.chaleil@gmail.com>
2017-04-04 20:04:02 +0200

    Since api.yifysubtitle is gone test if the websit is alive.

--- test/api-alive.js
+++ test/api-alive.js
@@ -1,8 +1,8 @@
 import test from 'ava';
 import got from 'got';
 
-test(`http://api.yifysubtitles.com should be alive`, async t => {
-	const res = await got(`http://api.yifysubtitles.com`);
+test(`http://yifysubtitles.com should be alive`, async t => {
+	const res = await got(`http://yifysubtitles.com`);
 
 	t.is(res.statusCode, 200);
 });
