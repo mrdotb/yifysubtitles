653d8ea8a1a3702fad0277c0f0bb50a074e1f150
mrdotb <mrdotb@protonmail.com>
2020-01-30 12:09:52 +0900

    xo xo & cleanup & bump patch version

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const fs = require("fs");
-const path = require("path");
-const got = require("got");
-const pMap = require("p-map");
-const streamz = require("streamz");
-const unzipper = require("unzipper").Parse;
-const srt2vtt = require("srt-to-vtt");
-const cheerio = require("cheerio");
+const fs = require('fs');
+const path = require('path');
+const got = require('got');
+const pMap = require('p-map');
+const streamz = require('streamz');
+const unzipper = require('unzipper').Parse;
+const srt2vtt = require('srt-to-vtt');
+const cheerio = require('cheerio');
 
-const langsFormat = require("./langs");
+const langsFormat = require('./langs');
 
-const apiUri = "http://api.yifysubtitles.com/subs";
-const uri = "http://www.yifysubtitles.com/movie-imdb";
-const downloadUri = "http://yifysubtitles.com";
+// Down const apiUri = 'http://api.yifysubtitles.com/subs';
+const uri = 'https://www.yifysubtitles.com/movie-imdb';
+const downloadUri = 'https://yifysubtitles.com';
 const langK = Object.keys(langsFormat);
 const langV = langK.map(i => langsFormat[i]);
 
@@ -23,20 +23,20 @@ const scrape = imdbId => {
   return got(`${uri}/${imdbId}`)
     .then(res => cheerio.load(res.body))
     .then($ => {
-      return $("tbody tr")
+      return $('tbody tr')
         .map((i, el) => {
           const $el = $(el);
           return {
-            rating: $el.find(".rating-cell").text(),
+            rating: $el.find('.rating-cell').text(),
             language: $el
-              .find(".flag-cell .sub-lang")
+              .find('.flag-cell .sub-lang')
               .text()
               .toLowerCase(),
             url:
-              $el
-                .find(".download-cell a")
-                .attr("href")
-                .replace("subtitles/", "subtitle/") + ".zip"
+            $el
+              .find('.download-cell a')
+              .attr('href')
+              .replace('subtitles/', 'subtitle/') + '.zip'
           };
         })
         .get();
@@ -48,17 +48,18 @@ const langFilter = (subs, langs) => {
     const lang = subs
       .filter(s => s.language === l)
       .sort((a, b) => b.rating - a.rating);
-    if (lang.length) {
+    if (lang.length > 0) {
       acc[l] = lang[0];
     }
+
     return acc;
   }, {});
   return data;
 };
 
 const downloadFormat = format => (lang, url, link) => {
-  let writed = "";
-  let fullPath = "";
+  let writed = '';
+  let fullPath = '';
 
   return got
     .stream(downloadUri + url)
@@ -69,22 +70,23 @@ const downloadFormat = format => (lang, url, link) => {
         // Add Language to subtitle name and deete spaces
         const escapedLang = lang.replace('/', '-');
         entry.path = entry.path
-          .replace(/\s+/g, ".")
+          .replace(/\s+/g, '.')
           .replace(parsedPath.ext, `_${escapedLang}_${parsedPath.ext}`);
-        if (parsedPath.dir === "" && parsedPath.ext === ".srt") {
+        if (parsedPath.dir === '' && parsedPath.ext === '.srt') {
           writed =
-            format === "srt" ? entry.path : entry.path.replace("srt", "vtt");
+            format === 'srt' ? entry.path : entry.path.replace('srt', 'vtt');
           fullPath = path.join(link, writed);
-          return format === "srt"
-            ? entry.pipe(fs.createWriteStream(fullPath))
-            : entry.pipe(srt2vtt()).pipe(fs.createWriteStream(fullPath));
+          return format === 'srt' ?
+            entry.pipe(fs.createWriteStream(fullPath)) :
+            entry.pipe(srt2vtt()).pipe(fs.createWriteStream(fullPath));
         }
+
         entry.autodrain();
       })
     )
     .promise()
     .then(() => ({
-      lang: lang,
+      lang,
       langShort: formatLangShort(lang),
       path: fullPath,
       fileName: writed
@@ -93,7 +95,7 @@ const downloadFormat = format => (lang, url, link) => {
 
 const downloads = (res, opts) => {
   const download = downloadFormat(opts.format);
-  const { concurrency, path } = opts;
+  const {concurrency, path} = opts;
 
   return pMap(
     Object.keys(res),
@@ -112,21 +114,21 @@ const yifysubtitles = (imdbId, opts) => {
   opts = Object.assign(
     {
       path: __dirname,
-      langs: ["en"],
+      langs: ['en'],
       concurrency: Infinity,
-      format: "vtt"
+      format: 'vtt'
     },
     opts
   );
 
   if (opts.langs.constructor !== Array) {
-    throw new TypeError("Expected `langs` to be an array");
+    throw new TypeError('Expected `langs` to be an array');
   } else if (opts.langs.some(lang => langK.indexOf(lang) === -1)) {
     throw new TypeError(`Expected \`langs\` members to be in ${langK}`);
   }
 
   return scrape(imdbId).then(res =>
-    res.length ? runConditional(imdbId, opts, res) : []
+    (res.length > 0) ? runConditional(imdbId, opts, res) : []
   );
 };
 
